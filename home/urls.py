039d4c65dc6260028ca7d6b84c6df9b9b32fafbf
Kanishk Tihaiya <kanishk.tihaiya@gmail.com>
2025-02-07 20:20:35 -0500

    Chapter 12 changes

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.index, name='home.index'),
     path('about', views.about, name='home.about'),
-
-    path('movies/', include('movies.urls')),
 ]
--- moviesstore/urls.py
+++ moviesstore/urls.py
@@ -2,7 +2,7 @@
 URL configuration for moviesstore project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -18,11 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('movies/', include('movies.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('cart/', include('cart.urls')),
 ]
+
 urlpatterns += static(settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT)
